ff35a9a600918adbdb2461aabdf2d63d1dc6b4cb
trusktr <trusktr@gmail.com>
2012-10-09 00:12:47 -0700

    Added header/preamble --> assn5-1.c

--- assn5/assn5-1.c
+++ assn5/assn5-1.c
@@ -1,4 +1,6 @@
-
+/*
+ * Joe Pea -- Assignment 4 -- Weide Change
+ */
 
 
 
@@ -11,8 +13,6 @@
 #include <signal.h> // to use SIGKILL
 
 #define MAX_ROUNDS 10
-#define MAX_CHILDREN 10
-#define MAX_SUM 100
 
 
 
@@ -31,35 +31,33 @@ int main(int argc, char *args[]) {
 		printf("Usage: %s arg1 arg2\n", args[0]);
 		exit(1);
 	}
-printf("### DEBUG: %i\n", 0);
 
 	numberOfChildren = atoi(args[1]);
 	minFinalSum = atoi(args[2]);
-printf("### DEBUG: %i\n", 1);
 	
-	if (numberOfChildren < 1 || minFinalSum < 1 numberOfChildren > 10 || minFinalSum > 100) {
+	if (numberOfChildren < 1 || minFinalSum < 1 || numberOfChildren > 10 || minFinalSum > 100) {
 		printf("Requirement: the arguments must be greater than 0. First arg no greater than 10. Second arg no greater than 100.\n");
 		exit(2);
 	}
-printf("### DEBUG: %i\n", 2);
 	
 	pid_t fork_pids[numberOfChildren]; // this is easier than declaring a global then using malloc or something.
 	pid_t wait_pid, exit_status;
-	int* outgoingPipes[numberOfChildren]; // array of pipes.
 	int randNumber = 0;
-printf("### DEBUG: %i\n", 3);
+	int* outgoingPipes[numberOfChildren]; // array of pipes.
 	
 	/*
 	 * Create the three children processes.
 	 */
 	unsigned int i=0;
 	for (i=0; i<numberOfChildren; i++) {
-		fork_pids[i] = fork();
-		outgoingPipes[i] = (int*)malloc( sizeof(int) * 2 );
+		outgoingPipes[i] = (int*)malloc( sizeof(int) * 2 ); // make the pipes.
 		if ( pipe(outgoingPipes[i]) == -1 ) {
 			printf("Error making pipe for child %i. \n", i);
 			exit(3);
 		}
+	}
+	for (i=0; i<numberOfChildren; i++) { // fork the children
+		fork_pids[i] = fork();
 		
 		if (fork_pids[i] == -1) { // -1 means error forking.
 			printf("Error forking. \n"); // out of memory?
@@ -69,24 +67,9 @@ printf("### DEBUG: %i\n", 3);
 			MyChild(i, outgoingPipes);
 		}
 	}
-
-/*sleep(5);*/
-printf("\n\n### DEBUG: sizeof(outgoingPipes): %i\n", sizeof(outgoingPipes));
-int x;
-for (x=0; x<numberOfChildren; x++) {
-	printf("    sizeof pipe #%i -->> %i\n", x, sizeof(outgoingPipes[x]));
-	int y;
-	for (y=0; y<2; y++) {
-		printf("       fileno: %i\n\n", outgoingPipes[x][y]);
-	}
-}
-
-printf("### DEBUG: %i\n", 4);
 	
-	sleep(1);
 	/*Let's initiate randomness based on the current time. */
 	srand( (unsigned int)time(NULL) );
-printf("### DEBUG: %i\n", 5);
 	
 	for (i=0; i<MAX_ROUNDS; i++) {
 		int j=0;
@@ -107,7 +90,6 @@ printf("### DEBUG: %i\n", 5);
 			usleep(1000);
 		}
 	}
-printf("### DEBUG: %i\n", 6);
 	
 	/*
 	 * Wait for the children to exit.
@@ -117,7 +99,6 @@ printf("### DEBUG: %i\n", 6);
 			if ( fork_pids[i] != wait_pid )
 				kill(fork_pids[i], SIGKILL);
 		}
-printf("### DEBUG: %i\n", 7);
 		
 		// exit code is only given 8-bit space, packed in 3rd byte
 		exit_status <<= 16; exit_status >>= 24;
@@ -129,29 +110,12 @@ printf("### DEBUG: %i\n", 7);
 
 void MyChild(int i, int** pipes) {
 	int randNum = 0; // will receive from parent through pipe.
-
-if (i==0) {
-printf("### DEBUG: %i\n", 8);
-printf("\n\n### DEBUG: sizeof(pipes): %i\n", sizeof(pipes));
-int x;
-for (x=0; x<3; x++) {
-	printf("    sizeof pipe #%i -->> %i\n", x, sizeof(pipes[x]));
-	int y;
-	for (y=0; y<2; y++) {
-		printf("       fileno: %i\n\n", pipes[x][y]);
-	}
-}
-}
 	
 	while( sum < minFinalSum ) {
-		usleep(100);
-printf("### DEBUG: %i\n", 9);
 		// get random number between 0 and 9 from pipe.
 		read( pipes[i][0], &randNum, sizeof(int) ); // waits for the parent to write to the pipe:
-printf("### DEBUG: %i\n", 10);
 		sum = sum + randNum; //increment sum by random number.
-		printf("Child #%u (PID %u) receives the number %i. -- Current sum: %u\n", i, getpid(), randNum, sum); //display child proc id and current sum
-printf("### DEBUG: %i\n", 11);
+		printf("Child #%u (PID %u) receives the number %i. Sum = %u\n", i, getpid(), randNum, sum); //display child proc id and current sum
 
 		// bonus part: write current sum to another pipe (parent reads)
 		// can sleep for 1 second in bonus version
